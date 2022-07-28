c1d41085f357f1d84ebe9d60611913ba67522ddf
“rajatt95” <rajatvermaa95@gmail.com>
2022-07-28 22:40:34 +0530

    Github actions setup completed

--- tests/Section_09_RS_UI_API_Tests/41_Intercept_API_Block_NetworkCalls.spec.js
+++ tests/Section_09_RS_UI_API_Tests/41_Intercept_API_Block_NetworkCalls.spec.js
@@ -10,7 +10,7 @@
 
 const {test,expect} = require('@playwright/test');
 
-test('Section_09_RS_UI_API_Tests - RahulShettyAcademy Client App Login - Intercept_API_Block_NetworkCalls', async ({page})=> {
+test('Section_09_RS_UI_API_Tests - RahulShettyAcademy Client App Login - Intercept_API_Block_NetworkCalls - BLOCK', async ({page})=> {
    
     
     // Intercept -> Block Network call
@@ -26,6 +26,24 @@ test('Section_09_RS_UI_API_Tests - RahulShettyAcademy Client App Login - Interce
     await page.goto('https://www.rahulshettyacademy.com/loginpagePractise/');
     
 });
+
+test('Section_09_RS_UI_API_Tests - RahulShettyAcademy Client App Login - Intercept_API_Block_NetworkCalls - DO NOT BLOCK', async ({page})=> {
+   
+    
+    // // Intercept -> Block Network call
+    // await page.route(
+    //     // Any Request URL which ends with CSS
+    //     // We are blocking the CSS to be loaded in Browser
+    //     '**/*.css',
+
+    //     // abort() -> it will stop the API call to reach to Browser 
+    //     route => route.abort()
+    // );
+    
+    await page.goto('https://www.rahulshettyacademy.com/loginpagePractise/');
+    
+});
+
 
 test.afterEach(async() => {
     await waitForSomeTime(6);    
